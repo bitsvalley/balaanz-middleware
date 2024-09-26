8f84d13b3aee34565b41fc250f33fd93fc33805a
raj kumar <rajmetti@gmail.com>
2024-09-26 21:16:05 +0530

    initial commit

---
+++ src/main/java/com/balaanz_middleware/app/BalaanzMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.balaanz_middleware.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalaanzMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalaanzMiddlewareApplication.class, args);
+	}
+
+}
